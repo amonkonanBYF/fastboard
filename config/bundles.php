f030e6595ad32790b2431eeb7d6eac86e5356dd7
amonkonanBYF <fabianbrin@gmail.com>
2020-12-29 04:52:16 -0800

    (feat) base front-end home

--- config/bundles.php
+++ config/bundles.php
@@ -14,4 +14,5 @@ return [
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
